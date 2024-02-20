70978003a19d6b11af56b5c8433e11001a1c6996
Thomas <z5311865@ad.unsw.edu.au>
2024-02-20 21:47:03 +1100

    Added adminAuthRegister stub

--- src/auth.js
+++ src/auth.js
@@ -1,7 +1,11 @@
-/* 
- * Function stub for adminAuthRegister
- * Register a user with an email, password, and names,
- * then returns their authUserId.
+/**
+ * Register a user with an email, password, and names, then returns their authUserId.
+ * @param {string} email - User's email
+ * @param {string} password - User's password
+ * @param {string} nameFirst - User's first name
+ * @param {string} nameLast - User's last name
+ * 
+ * @param {integer} authUserId - unique identifier for an academic
  */
 function adminAuthRegister(email, password, nameFirst, nameLast) {
     return {
