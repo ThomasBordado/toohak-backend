735383bdd66cc04def9808ab6d0c218b849ba947
Thomas Bordado <z5311865@ad.unsw.edu.au>
2024-02-22 13:03:19 +1100

    Updated adminAuthLogin function comment

--- src/auth.js
+++ src/auth.js
@@ -5,7 +5,7 @@
  * @param {string} nameFirst - User's first name
  * @param {string} nameLast - User's last name
  * 
- * @returns {number} authUserId - unique identifier for an academic, registering with email, password and name.
+ * @returns {authUserId} - unique identifier for an academic, registering with email, password and name.
  */
 function adminAuthRegister(email, password, nameFirst, nameLast) {
     return {
@@ -18,7 +18,7 @@ function adminAuthRegister(email, password, nameFirst, nameLast) {
  * @param {string} email - User's email
  * @param {string} password - User's password
  * 
- * @returns {number} authUserId - unique identifier for an academic, given email and password
+ * @returns {authUserId} - unique identifier for an academic, given email and password
  */
 function adminAuthLogin(email, password) {
     return {
