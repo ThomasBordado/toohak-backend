b47d1cd967e3acc9ad2143b6d5adb14dff52a389
Ethan McGregor <z5481290@ad.unsw.edu.au>
2024-02-22 13:06:09 +1100

    Adding stub code for clear

---
+++ src/other.js
@@ -0,0 +1,10 @@
+/**
+ * Provides a list of all quizzes that are owned by the currently logged in user
+ * @param {} - clears the state of the application back to the start
+ * @returns {} - clears the state of the application back to the start
+ */
+function clear() {
+    return {
+      
+    };
+  }
