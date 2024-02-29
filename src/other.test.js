a690c59cb977d1901509a3230902a6bdd5e4a4ad
Thomas <z5311865@ad.unsw.edu.au>
2024-02-29 16:02:29 +1100

    edited tests to account for userIdStore and quizIdStore counters

--- src/other.test.js
+++ src/other.test.js
@@ -9,13 +9,18 @@ beforeEach(()=> {
 test('Test clear registered user', () => {
     expect(clear()).toStrictEqual({});
     let data = getData();
-    expect(data).toMatchObject({users: [],quizzes: []});
+    expect(data).toStrictEqual({users: [], quizzes: [], userIdStore: 0, quizIdStore: 0});
     adminAuthRegister('hayden.smith@unsw.edu.au', 'password1', 'Hayden', 'Smith');
     data = getData();
-    expect(data).toMatchObject({users: [{userId: 1, nameFirst: 'Hayden', nameLast: 'Smith', email: 'hayden.smith@unsw.edu.au', password: 'password1', numSuccessfulLogins: 0, numFailedPasswordsSinceLastLogin: 0, quizzes: []}],quizzes: []});
+    expect(data).toStrictEqual({
+        users: [{userId: 1, nameFirst: 'Hayden', nameLast: 'Smith', email: 'hayden.smith@unsw.edu.au', password: 'password1', numSuccessfulLogins: 0, numFailedPasswordsSinceLastLogin: 0, quizzes: []}], 
+        quizzes: [], 
+        userIdStore: 0, 
+        quizIdStore: 0
+    });
     clear();
     data = getData();
-    expect(data).toMatchObject({users: [],quizzes: []});
+    expect(data).toStrictEqual({users: [], quizzes: [], userIdStore: 0, quizIdStore: 0});
 });
 
 // Add a test to clear quizzes when we are able to make quizzes.
