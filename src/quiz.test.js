f350fd0d354778b1764fdb759be73ca233457efc
Chloe Turner <z5478701@ad.unsw.edu.au>
2024-02-29 13:48:19 +1100

    testing stubs for quiz.js

---
+++ src/quiz.test.js
@@ -0,0 +1,37 @@
+import {adminQuizList,
+        adminQuizCreate,
+        adminQuizRemove,
+        adminQuizInfo,
+        adminQuizNameUpdate,
+        adminQuizDescriptionUpdate,} from './quiz.js'
+
+beforeEach(() => {
+    clear();
+});
+
+
+describe('adminQuizList testing', () => {
+
+});
+
+describe('adminQuizCreate testing', () => {
+
+});
+
+describe('adminQuizRemove testing', () => {
+
+});
+
+describe('adminQuizInfo testing', () => {
+
+});
+
+describe('adminQuizNameUpdate testing', () => {
+
+});
+
+describe('adminQuizDescriptionUpdate testing', () => {
+
+});
+  
+          
