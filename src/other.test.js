8b6d9d5b312ec686cd8b81801eeb52b084311697
Thomas <z5311865@ad.unsw.edu.au>
2024-02-29 14:41:48 +1100

    added two tests for clear basical function

---
+++ src/other.test.js
@@ -0,0 +1,21 @@
+import { clear } from './other.js';
+import { adminAuthRegister } from './auth.js';
+import { getData } from './dataStore.js';
+
+beforeEach(()=> {
+    clear();
+})
+
+test('Test clear registered user', () => {
+    expect(clear()).toStrictEqual({});
+    let data = getData();
+    expect(data).toMatchObject({users: [],quizzes: []});
+    adminAuthRegister('hayden.smith@unsw.edu.au', 'password1', 'Hayden', 'Smith');
+    data = getData();
+    expect(data).toMatchObject({users: [{userId: 1, nameFirst: 'Hayden', nameLast: 'Smith', email: 'hayden.smith@unsw.edu.au', password: 'password1', numSuccessfulLogins: 0, numFailedPasswordsSinceLastLogin: 0, quizzes: []}],quizzes: []});
+    clear();
+    data = getData();
+    expect(data).toMatchObject({users: [],quizzes: []});
+});
+
+test.todo('Test clear quizzes');
