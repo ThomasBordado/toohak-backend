14f5b2ef41ee1a8fa640cbdc4cde292c882bf50a
Yimei Hu <z5489084@ad.unsw.edu.au>
2024-03-19 21:47:10 +1100

    delete import function

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,6 @@ import sui from 'swagger-ui-express';
 import fs from 'fs';
 import path from 'path';
 import process from 'process';
-import { adminUserDetailsUpdate } from './auth';
 
 // Set up web app
 const app = express();
