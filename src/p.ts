c3caf43d2f76cc6fb3501b2951e742aa38de486d
Yimei Hu <z5489084@ad.unsw.edu.au>
2024-03-26 23:58:32 +1100

    get helper functio

---
+++ src/p.ts
@@ -0,0 +1,28 @@
+import * as fs from 'fs';
+import { getData, setData } from './dataStore';
+// ========================================================================= //
+// Functionality for persistence
+// Function to save data from dataStore to a JSON file
+export const saveData = (): void => {
+  try {
+    fs.writeFileSync('./database.json', JSON.stringify(getData()));
+    console.log('Data saved successfully to ./database.json');
+  } catch (error) {
+    console.error('Error saving data to file: ./database.json', error);
+  }
+};
+
+// Function to update dataStore from a JSON file
+export const loadData = (): void => {
+  if (!fs.existsSync('./database.json')) {
+    return;
+  }
+  try {
+    const jsonData = fs.readFileSync('./database.json', 'utf8');
+    const data = JSON.parse(jsonData);
+    setData(data);
+    console.log('DataStore updated from ./database.json');
+  } catch (error) {
+    console.error('Error updating dataStore from file: ./database.json');
+  }
+};
