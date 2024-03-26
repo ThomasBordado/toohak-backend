b3fcd9f81443b73c035f6656b3fc1c804a47fe86
Jared Simion <z5419285@ad.unsw.edu.au>
2024-03-26 22:32:27 +1100

    Completed routing for adminQuizDescriptionUpdate

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ import fs from 'fs';
 import path from 'path';
 import process from 'process';
 import { clear } from './other';
-import { adminQuizList, adminQuizCreate, adminQuizRemove } from './quiz';
+import { adminQuizList, adminQuizCreate, adminQuizRemove, adminQuizDescriptionUpdate } from './quiz';
 import { adminAuthLogin, adminAuthRegister } from './auth';
 
 // Set up web app
@@ -75,6 +75,23 @@ app.post('/v1/admin/quiz', (req: Request, res: Response) => {
   if ('error' in result) {
     if (result.error.localeCompare('Token is empty or invalid') === 0) {
       return res.status(401).json(result);
+    }
+    return res.status(400).json(result);
+  }
+  res.json(result);
+});
+
+app.put('/v1/admin/quiz/:quizid/description', (req: Request, res: Response) => {
+  // Everything in req.body will be of the correct type
+  const quizId = parseInt(req.params.quizid as string);
+  const token = parseInt(req.body.token as string);
+  const { newDescription } = req.body;
+  const result = adminQuizDescriptionUpdate(token, quizId, newDescription);
+  if ('error' in result) {
+    if (result.error.localeCompare('Token is empy or invalid') === 0) {
+      return res.status(401).json(result);
+    } else if (result.error === 'Invalid quizId' || result.error === 'User does not own quiz') {
+      return res.status(403).json(result);
     }
     return res.status(400).json(result);
   }
