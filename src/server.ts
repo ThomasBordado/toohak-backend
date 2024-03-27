f449ae3371e8b01aaec72e961cc45e3a299154f0
Yimei Hu <z5489084@ad.unsw.edu.au>
2024-03-27 12:57:49 +1100

    remove unnecessary file

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ import process from 'process';
 import { clear } from './other';
 import { adminQuizList, adminQuizCreate, adminQuizRemove } from './quiz';
 import { adminAuthLogin, adminAuthRegister, adminUserDetailsUpdate } from './auth';
-import { saveData } from './p';
+import { saveData } from './t';
 
 // Set up web app
 const app = express();
