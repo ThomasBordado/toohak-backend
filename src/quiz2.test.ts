404c03fbc405619d1a0e618ad648839184d56705
Yimei Hu <z5489084@ad.unsw.edu.au>
2024-04-18 12:20:53 +1000

    update END session checking test for quizTransfer

--- src/quiz2.test.ts
+++ src/quiz2.test.ts
@@ -1482,6 +1482,12 @@ describe('Testing Post /v2/admin/quiz/{quizid}/transfer', () => {
     test('Quiz ID refers to a quiz that has a name that is already used by the target user', () => {
       expect(() => requestquizTransfer(user2.token, 'validemail@gmail.com', quiz3.quizId)).toThrow(HTTPError[400]);
     });
+
+    test('Error test for 400 error, Any session for this quiz is not in END state', () => {
+      // const session = requestSessionStart(user.token,quiz.quizId, 3);
+      // expect(() => requestquizTransfer(user2.token, 'validemail@gmail.com', quiz3.quizId)).toThrow(HTTPError[400]);
+    });
+
   });
 
   describe('Error test for 401 error', () => {
